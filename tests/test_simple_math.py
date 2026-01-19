642f7db207fd270dc441bd01cc083c54b18d9d51
kithor <g.soulichanh@gmail.com>
2026-01-19 22:50:46 +0100

    add soustraction with unit test

--- tests/test_simple_math.py
+++ tests/test_simple_math.py
@@ -6,6 +6,9 @@ class TestSimpleMath(unittest.TestCase):
     def test_addition(self):
         self.assertEqual(SimpleMath.addition(2, 3), 5)
 
+    def test_soustraction(self):
+        self.assertEqual(SimpleMath.soustraction(10, 4), 6)
+
 
 if __name__ == "__main__":
     unittest.main()
